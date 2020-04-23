ef9cdd073ea199d2331e2b1b2eff16602c222898
Falcon1991 <dfalkinburg@gmail.com>
2020-04-23 15:43:04 -0500

    Initial

---
+++ Civic/Civic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Civic
+//
+//  Created by Dillon Falkinburg on 4/23/20.
+//  Copyright © 2020 Dillon Falkinburg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Civic/Civic/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Civic
+//
+//  Created by Dillon Falkinburg on 4/23/20.
+//  Copyright © 2020 Dillon Falkinburg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
